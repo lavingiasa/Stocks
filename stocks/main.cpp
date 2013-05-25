3ad065a8cbef5fadb04995262c14c0c8be12bc04
Samir Lavingia <lavingiasa@Samirs-MacBook-Pro.local>
2013-05-25 13:34:13 -0700

    Initial Commit

---
+++ stocks/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  stocks
+//
+//  Created by Samir Lavingia on 5/25/13.
+//  Copyright (c) 2013 Samir Lavingia. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
